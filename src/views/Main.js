32278b40d04104c459b085d9ce8e368c86cc354b
Greg <gpritch84@yahoo.com>
2020-11-10 14:34:01 -0600

    Part 1 of the Product Manager project

---
+++ src/views/Main.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import ProductForm from '../components/ProductForm';
+
+export default () => {
+
+    return (
+        <div>
+            <h1>Product Manager</h1>
+            <ProductForm/>
+        </div>
+    )
+}
